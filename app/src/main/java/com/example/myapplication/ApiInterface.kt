2a3f30c2b8ef0ddfd757bf7412b7ed1a933c4476
Anjal Saneen <anjalsaneen@Anjals-MacBook-Pro.local>
2021-07-09 07:54:02 +0530

    Added retrofit

---
+++ app/src/main/java/com/example/myapplication/ApiInterface.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+import androidx.annotation.Keep
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiInterface {
+
+    @POST("v1/GetProfile")
+    fun getAB(@Body req: GetProfileRequest) : Call<GetProfileResponse>
+
+    companion object {
+
+        var BASE_URL = "https://ab.staging.okcredit.in/"
+
+        fun create() : ApiInterface {
+
+            val retrofit = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build()
+            return retrofit.create(ApiInterface::class.java)
+
+        }
+
+    }
+}
+
+@Keep
+data class GetProfileRequest(
+    val merchant_id: String,
+    val device_id: String
+)
+
+@Keep
+data class GetProfileResponse(
+    val profile: Profile
+)
+
+@Keep
+data class Profile(
+    val merchant_id: String,
+    val features: Map<String, Boolean>,
+    val experiments: Map<String, Experiment>
+)
+
+@Keep
+data class Experiment(
+    val name: String,
+    val status: Int,
+    val variant: String,
+    val vars: Map<String, String>
+)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,51 +1,94 @@
-//package com.example.myapplication
-//
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.appcompat.app.AppCompatActivity
-//import com.google.gson.JsonObject
-//import okhttp3.*
-//import java.io.IOException
-//
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//
-//
-//    fun callMethod() {
-//        val client = OkHttpClient()
-//
-//        val postData = JsonObject()
-//        postData.addProperty("name", "morpheus")
-//        postData.addProperty("job", "leader")
-//
-//        val JSON = MediaType.parse("application/json; charset=utf-8")
-//        val postBody: RequestBody = create(JSON, postData.toString())
-//        val post: Request = Request.Builder()
-//            .url("https://reqres.in/api/users")
-//            .post(postBody)
-//            .build()
-//
-//        client.newCall(post).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                e.printStackTrace()
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                try {
-//                    val responseBody = response.body
-//                    if (!response.isSuccessful) {
-//                        throw IOException("Unexpected code $response")
-//                    }
-//                    Log.i("data", responseBody!!.string())
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                }
-//            }
-//        })
-//
-//    }
-//}
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import java.io.IOException
+import java.util.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var txtString: TextView? = null
+
+    companion object {
+        val JSON = "application/json; charset=utf-8".toMediaType()
+
+        private var POST_URL_AB = "https://ab.staging.okcredit.in/v1/GetProfile"
+        private var AB_BODY = """{
+    "device_id": "86c20472-5ff6-4d23-b1f6-546bb041b7d3",
+    "merchant_id": ""
+}"""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.postOkhttp).setOnClickListener(this)
+        findViewById<View>(R.id.postRetrofit).setOnClickListener(this)
+        txtString = findViewById(R.id.txtString)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.postOkhttp -> try {
+                postRequestOkHttp(POST_URL_AB, AB_BODY)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            R.id.postRetrofit -> {
+                postRequestRetrofit()
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    fun postRequestOkHttp(postUrl: String, postBody: String) {
+        val client = OkHttpClient()
+        val body = RequestBody.create(JSON, postBody)
+        val request: Request = Request.Builder()
+            .url(postUrl)
+            .post(body)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                call.cancel()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    try {
+                        txtString!!.text = response.body!!.string()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        })
+    }
+
+
+    fun postRequestRetrofit(){
+        val apiInterface = ApiInterface.create().getAB(GetProfileRequest(merchant_id = "", device_id = UUID.randomUUID().toString()))
+
+        //apiInterface.enqueue( Callback<List<Movie>>())
+        apiInterface.enqueue(object:retrofit2.Callback<GetProfileResponse> {
+            override fun onResponse(
+                call: retrofit2.Call<GetProfileResponse>,
+                response: retrofit2.Response<GetProfileResponse>
+            ) {
+                try {
+                    txtString!!.text = response.body().toString()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(call: retrofit2.Call<GetProfileResponse>, t: Throwable) {
+            }
+
+        })
+    }
+}
